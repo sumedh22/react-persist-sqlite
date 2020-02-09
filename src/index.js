d9214f471d603e7e357308da53a8593f66c64768
Sumedh Chakravorty <sumedh@Sumedhs-MacBook-Pro.local>
2020-02-09 08:48:47 +0530

    added code for sqlite

--- src/index.js
+++ src/index.js
@@ -1,22 +1,56 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+/**
+ * Returns a function to avoid eager execution of window.sqlitePlugin.openDatabase
+ * 
+ */
+export const sqlLiteStorage = () => {
+  let db = window.sqlitePlugin.openDatabase({
+      name: 'redux-store-db',
+      location: 'default',
+  });
 
-import styles from './styles.css'
+  return {
+      getItem(key) {
+          return new Promise((resolve, reject) => {
+              try {
+                  db.transaction(tx => {
+                      tx.executeSql('CREATE TABLE IF NOT EXISTS redux_store (key, value)', []);
+                      tx.executeSql('SELECT value FROM redux_store WHERE key = ?', [key], (tx, rs) => {
+                          resolve(rs.rows.item(0).value)
+                      });
+                  }, err => {
+                      reject(err)
+                  }, res => {
+                      resolve(res)
+                  })
+              } catch (err) {
+                  console.error(err)
+                  reject(err)
+              }
+          })
+      },
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      setItem(key, value) {
+          return new Promise((resolve, reject) => {
+              db.transaction(tx => {
+                  tx.executeSql('UPDATE redux_store SET value = ? WHERE key = ?', [value,key]);
+              }, err => {
+                  reject(err)
+              }, res => {
+                  resolve(res)
+              })
+          })
+      },
+      removeItem(key) {
+          return new Promise((resolve, reject) => {
+              db.transaction(tx => {
+                  tx.executeSql('DELETE FROM redux_store WHERE key = ?', [key]);
+              }, err => {
+                  reject(err)
+              }, res => {
+                  console.log(res)
+                  resolve(res)
+              })
+          })
+      },
   }
 }
